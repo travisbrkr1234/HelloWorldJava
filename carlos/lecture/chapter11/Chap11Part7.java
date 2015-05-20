93083f48ac6ff5b4f05b00654ef895a5c0076272
travisbrkr1234 <carlos.ochoa@infusionsoft.com>
2015-05-20 10:17:22 -0700

    Completed chap11

---
+++ carlos/lecture/chapter11/Chap11Part7.java
@@ -0,0 +1,22 @@
+// searching for particular data in a data set
+import java.util.Random;
+import java.util.ArrayList;
+
+public class Chap11Part7 {
+  public static void main(String[] args)
+  {
+    Random rand = new Random(System.currentTimeMillis());
+    ArrayList<Integer> numbers = new ArrayList<Integer>();
+    for(int i = 0; i < 100; i++)
+        numbers.add(rand.nextInt(101));
+  /*  if (numbers.contains(100))
+        System.out.println("100 was found at position: " + numbers.indexOf(100));
+    else
+        System.out.println("100 was not found in numbers."); */
+        int min = 0;
+        for (int i = 1; i < numbers.size(); i++)
+            if (numbers.get(i) < numbers.get(min))
+            min = i;
+            System.out.println("The minimum value in numbers is: " + numbers.get(min));
+  }
+}
---
+++ carlos/lecture/chapter11/Chap11Part8.java
@@ -0,0 +1,23 @@
+//Sorting
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Chap11Part8 {
+  public static void main(String[] args)
+  {
+    Random rand = new Random(System.currentTimeMillis());
+    ArrayList<Integer> numbers = new ArrayList<Integer>();
+    for(int i = 0; i < 20; i++)
+        numbers.add(rand.nextInt(101));
+    display(numbers);
+    Collections.sort(numbers);
+    display(numbers);
+  }
+
+  static void display(ArrayList arr) {
+    for(int i = 0; i < arr.size(); i++)
+      System.out.print(arr.get(i) + " ");
+      System.out.println();
+}
+}
