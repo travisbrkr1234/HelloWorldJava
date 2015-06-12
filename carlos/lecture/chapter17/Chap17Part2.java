1308dcb75834926c2548d3945e715af7fa01e33b
travisbrkr1234 <carlos.ochoa@infusionsoft.com>
2015-06-12 07:46:37 -0700

    Completed Lecture

---
+++ carlos/lecture/chapter17/Chap17Part2.java
@@ -0,0 +1,14 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+public class Chap17Part2 {
+   public static void main(String[] args) throws IOException
+   {
+      String fileName = "grades.txt";
+      PrintWriter outFile = new PrintWriter(new FileWriter(fileName));
+      outFile.println(85);
+      outFile.println(77);
+      outFile.close();
+   }
+}
